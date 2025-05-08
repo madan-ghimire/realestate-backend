3d2ab1f10fa29e1eef092311081b51ba885a7c84
madanghimire-3ptrades <madanghimire-3ptrades@3ptrades.com>
2025-05-08 12:21:13 +0545

    add role based access control

--- src/middlewares/authorizedRoles.ts
+++ src/middlewares/authorizedRoles.ts
@@ -4,7 +4,7 @@ export const authorizeRoles = (...roles: string[]) => {
   return (req: Request, res: Response, next: NextFunction): void => {
     const user = (req as any).user;
 
-    console.log("check user here i am 🚀", user);
+    console.log("check user here i am 🚀 🚀", user);
 
     if (!user || !roles.includes(user.role)) {
       res.status(403).json({ message: "Forbidden: insufficient role" });
